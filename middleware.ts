a89d1d1cb51664c0125e227d1d04f95d3235146a
Diego <diegohxxiv@gmail.com>
2024-05-08 15:46:28 -0700

    fix: middleware run 3

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/users(.*)",
+  "/feed(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
